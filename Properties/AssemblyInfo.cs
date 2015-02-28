8699dbd012edd92913b7b0cf116641ae94ab1ae8
Jeroen <jeroen.developer@outlook.com>
2015-02-28 15:10:11 +0100

    Cleanup

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿// Copyright© 2015 Jeroen Stemerdink. All Rights Reserved.
+// 
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("EPi.Libraries.UnitTests.Base")]
 [assembly: AssemblyDescription("Base library for unit testing EPiServer with FakeItEasy")]
 [assembly: AssemblyConfiguration("")]
@@ -18,12 +41,12 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
+[assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("c4cafd5d-be35-48d7-ae1a-8a0fba80efe1")]
 
 // Version information for an assembly consists of the following four values:
@@ -36,5 +59,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
